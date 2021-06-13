63511bfee4695579d9a9551e1f9f985a4a286911
Paul Tuck <pau1tuck@outlook.com>
2021-06-14 01:37:12 +0800

    "2021-06-14T01:37CST"

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,32 @@
 import React from "react";
 import "./App.css";
+import {
+    Avatar,
+    Box,
+    Container,
+    Button,
+    Grid,
+    Link,
+    Paper,
+    TextField,
+    Typography,
+    Zoom,
+} from "@material-ui/core";
 
 function App(): JSX.Element {
     return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div>
+            <Container component="main" maxWidth="xs">
+                <Paper elevation={3}>
+                    <Box height="100%" padding={2}>
+                        <Box textAlign="center">
+                            <Typography component="h1" variant="h5">
+                                React Address Form
+                            </Typography>
+                        </Box>
+                    </Box>
+                </Paper>
+            </Container>
         </div>
     );
 }
